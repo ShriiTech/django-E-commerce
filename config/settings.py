07612e1f87ca352c47d33ab300bfcc54f4050468
ShriiTech <s.k.1381.2002@gamil.com>
2026-02-23 20:37:35 +0330

    config of postgresql

--- config/settings.py
+++ config/settings.py
@@ -119,8 +119,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres_django',
+        'USER': 'django_user',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
